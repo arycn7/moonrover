04250a569fdd37aee9d19165a2d8232e5c64cb04
arycn7 <79794566+arycn7@users.noreply.github.com>
2025-04-01 16:32:32 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,5 +1,4 @@
 import time
-import random
 
 class BandwidthLimiter:
     def __init__(self, max_bps):
@@ -17,13 +16,3 @@ class BandwidthLimiter:
                 self.bytes_sent += data_size
                 return
             time.sleep(0.1)
-
-def simulate_network_conditions():
-    """Call this in channel_simulator to add variability"""
-    global MOON_DELAY, PACKET_LOSS_PROB
-    while True:
-        MOON_DELAY = (
-            random.uniform(1.0, 3.0),
-            random.uniform(1.0, 3.0))
-        PACKET_LOSS_PROB = random.uniform(0.2, 0.4)
-        time.sleep(10)
